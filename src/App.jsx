cc4a211253a8c5b11894568f261538bb096e5d63
oliveiratullio <105726160+oliveiratullio@users.noreply.github.com>
2023-10-12 12:06:36 -0300

    criação do form e integração da API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
+import axios from 'axios'
 import { useState } from 'react'
 import styled from 'styled-components'
 
 function App() {
+  const apiKey = "e737a6a8a92d552a200e410bfb86db23"
+ 
+  const [city, setCity] = useState('')
+
+  function cityChange(e){
+    setCity(e.target.value)
+  }
+  
+  function search(e){
+    e.preventDefault()
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    const promise = axios.get(api)
+    promise.then(res =>{
+      console.log(res.data)
+    })
+    promise.catch((err) => console.log(err.response.data))
+  }
+
   return (
-    <Container>
-      <TopBar> Levo um casaquinho?
-        
+    <Container> 
+      <TopBar> 
+        Levo um casaquinho?
       </TopBar>
+      <SearchBar>
+        <FormContainer onSubmit={search}>
+          <input value={city} onChange={cityChange} required placeholder='Cidade' ></input>
+          <button type="submit" >Buscar</button>
+        </FormContainer>
+      </SearchBar>
     </Container>
   )
-
   
 }
 
@@ -35,4 +59,25 @@ const TopBar = styled.div`
   font-style: bold;
   margin-top: 40px;
 
+`
+const SearchBar = styled.div`
+  width: 100%;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  margin-top: 25px;
+`
+const FormContainer = styled.form`
+  width: 100%;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  input{
+    width: 200px;
+    font-size: 20px;
+  } 
+  button{
+    width: 100px;
+    font-size: 18px;
+  }
 `
